ab477b2d47f827c65b6001418e9fcbedaac92eab
Tina92 <1927601892@qq.com>
2016-12-08 21:46:12 +0800

    纹理贴图（待添加注释）

--- textture/texturedQuad.js
+++ textture/texturedQuad.js
@@ -3,7 +3,7 @@
  */
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
-    'attribute vec2 v_TexCoord;\n' +
+    'attribute vec2 a_TexCoord;\n' +
     'varying vec2 v_TexCoord;\n' +
     'void main() {\n' +
     '   gl_Position = a_Position;\n' +
@@ -11,7 +11,102 @@ var VSHADER_SOURCE =
     '}\n';
 var FSHADER_SOURCE =
     'precision mediump float;\n' +
-    'uniform sample2D u_Sampler;\n' +
+    'uniform sampler2D u_Sampler;\n' +
+    'varying vec2 v_TexCoord;\n' +
     'void main() {\n' +
     '   gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n' +
     '}\n';
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+    if(!gl){
+        console.log('Failed gl');
+        return;
+    }
+
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+        console.log('Failed init');
+        return;
+    }
+    //设置顶点信息
+    var n = initVertexBuffers(gl);
+    if(n<0){
+        console.log('Failed n');
+        return;
+    }
+    //配置纹理
+    if(!initTextures(gl, n)){
+        console.log('Failed Textures');
+        return;
+    }
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+function initVertexBuffers(gl) {
+    var verticesTexCoords = new Float32Array([
+        //顶点坐标，纹理坐标
+        -0.5, 0.5, 0.0, 1.0,
+        -0.5, -0.5, 0.0, 0.0,
+        0.5, 0.5, 1.0, 1.0,
+        0.5, -0.5, 1.0, 0.0,
+    ]);
+    var n = 4; //顶点数目
+    var vertexTexCoordBuffer = gl.createBuffer();
+    if(!vertexTexCoordBuffer){
+        console.log('Failed buffer');
+        return;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+
+    var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+    var a_Position = gl.getAttribLocation(gl.program,'a_Position');
+    if (a_Position<0){
+        console.log('Failed a_Position');
+        return;
+    }
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+    gl.enableVertexAttribArray(a_Position);
+    //将纹理坐标分配给a_TexCoord并开启它
+    var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+    if(a_TexCoord<0){
+        console.log('Failed a_TexCoord');
+        return;
+    }
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+    gl.enableVertexAttribArray(a_TexCoord);
+
+    return n;
+}
+function initTextures(gl, n) {
+    var texture = gl.createTexture();
+    //
+    var u_Sampler = gl.getUniformLocation(gl.program,'u_Sampler');
+    //
+    var image = new Image(); //
+    //
+    image.onload = function () {
+        loadTexture(gl, n, texture, u_Sampler, image);
+    };//
+    image.src='screenshot.png';
+
+    return true;
+
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image) {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); //
+    //
+    gl.activeTexture(gl.TEXTURE0);
+    //
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    //
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    //
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    //
+    gl.uniform1i(u_Sampler, 0);
+    //
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+}
